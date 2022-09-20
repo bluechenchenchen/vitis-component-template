644b0385f530fdff8dc04d356a8bc1c0b49e3f7a
heyu <1395294694@qq.com>
2022-09-20 10:04:27 +0800

    feat: 修改构建产物

--- template/packages/example/.umirc.ts
+++ template/packages/example/.umirc.ts
@@ -9,5 +9,23 @@ export default defineConfig({
   mode: 'site',
   webpack5: {},
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  history: {
+    type: 'hash'
+  },
+  styles: [
+    `
+    .__dumi-default-layout {
+      padding: 0px 108px 0 30px !important;
+    }
+    .__dumi-default-layout-toc {
+      padding: 0 !important;
+      width: 100px !important;
+    }
+    .__dumi-default-navbar,
+    .__dumi-default-menu {
+      display: none !important;
+    }
+    `
+  ]
   // more config: https://d.umijs.org/config
 });
--- template/packages/example/src/index.ts
+++ template/packages/example/src/index.ts
@@ -1,1 +1,3 @@
 export { default } from './component'
+// 如果该组件有个设置器，请将下面的代码取消注释
+// export { default as setters } from './setters'
