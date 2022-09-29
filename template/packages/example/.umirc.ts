cc6e9e8890ea35af5a749d064bbd4a373554e4ce
heyu <1395294694@qq.com>
2022-09-29 21:22:09 +0800

    feat: 用 vitis-material-parser 生成组件规格

--- template/packages/example/.umirc.ts
+++ template/packages/example/.umirc.ts
@@ -14,18 +14,24 @@ export default defineConfig({
   },
   styles: [
     `
-    .__dumi-default-layout {
-      padding: 0px 108px 0 30px !important;
-    }
-    .__dumi-default-layout-toc {
-      padding: 0 !important;
-      width: 100px !important;
-    }
-    .__dumi-default-navbar,
-    .__dumi-default-menu {
-      display: none !important;
-    }
-    `
+  .__dumi-default-layout {
+    padding: 14px 108px 0 30px !important;
+  }
+  .__dumi-default-layout-toc {
+    padding: 0 !important;
+    width: 100px !important;
+  }
+  .__dumi-default-navbar,
+  .__dumi-default-menu {
+    display: none !important;
+  }
+  .markdown h1 {
+    font-size: 22px;
+  }
+  .markdown h2 {
+    font-size: 14px;
+  }
+  `
   ]
   // more config: https://d.umijs.org/config
 });
--- template/packages/example/scripts/genAsset.mjs
+++ template/packages/example/scripts/genAsset.mjs
@@ -7,54 +7,12 @@ async function run() {
     const asset = await materialParser.default(componentPath, {
         workDir: process.cwd()
     })
-    const { componentConfig, version, description, name } = JSON.parse(fs.readFileSync( resolve(process.cwd(), 'package.json'), {encoding: 'utf-8'} ))
 
-    const assetPath = resolve(process.cwd(), 'asset', 'index.json')
+    const assetPath = resolve(process.cwd(), 'asset4', 'index.json')
     fs.ensureFileSync(assetPath)
-
     fs.writeFileSync(
         assetPath, 
-        JSON.stringify(
-            {
-              componentName: componentConfig.name,
-              packageName: name,
-              title: componentConfig.title,
-              iconUrl: componentConfig.iconUrl || '',
-              description,
-              docUrl:'todo',
-              version: version || '0.0.0',
-              props: asset ? asset.props: '',
-              advanced: {
-                // 组件的嵌套规则
-                nestingRule: {
-                  // 父级组件白名单
-                  // 业务组件必须放置在布局组件中
-                  parentWhitelist: ['LayoutColumn'],
-                  // 子组件白名单。
-                  // 空数组则说明其他组件不能放置在该组件中
-                  childWhitelist: []
-                },
-                supports: {
-                  // 是否能配置样式
-                  styles: true,
-                  // 是否能配置校验规则
-                  validation: false,
-                  // 是否能配置联动规则
-                  linkage: false,
-                  // 支持的事件列表
-                  events: ['onClick']
-                },
-                // 该组件是否是容器，比如 table 就是一个容器，它能单独从接口中获取自己的数据源
-                isContainer: false,
-                // 是否是表单组件
-                isFormControl: false,
-                // 是否是布局组件
-                isLayout: false
-              },
-            },
-            null,
-            2,
-        ),
+        asset
     )
 }
 
